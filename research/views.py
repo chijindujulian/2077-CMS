5f1bba8d59be6128973158f28d2cb454ef8cd7bf
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-07-17 07:10:19 +0100

    feat: queried Notion Database with status filters

--- research/views.py
+++ research/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from notion_client import Client
-import os
+import os, json, requests
 from django.http import JsonResponse
 
 # Create your views here.
@@ -9,5 +9,28 @@ def index(request):
 
 def researchPage(request):
     notion = Client(auth=os.environ['NOTION_TOKEN'])
-    data = notion.databases.query(os.environ.get('NOTION_DATABASE_ID'))
-    return JsonResponse(data)
+    data = notion.databases.query(os.environ.get('NOTION_DATABASE_ID'),
+    filter={
+        "property": "Status",
+        "status": {
+            "equals": "Done"
+        }
+    })
+    with open('data.json', 'w') as f:
+        json.dump(data, f, indent=4)
+    #return JsonResponse(data)
+    #print(data)
+    print("Printing list of Authors")
+    for result in data['results']:
+        authors = result['properties']['Author']['multi_select']
+        for author in authors:
+            print(author['name'])
+    data_to_render = data['results']
+    
+    print("Printing list of Titles")
+    for result1 in data_to_render:
+        titles = result1['properties']['Content Title']['title']
+        for title in titles:
+            print(title['plain_text'])                
+ 
+    return render(request, 'blogPostCard.html', {'data_to_render': data_to_render})
