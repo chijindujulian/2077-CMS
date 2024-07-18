6762271cdccfddcbfe4a5370208d90cb9cd5b922
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-07-18 22:04:13 +0100

    Feat: Api setup for the articles

--- main2077cms/settings.py
+++ main2077cms/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'research',
+    'rest_framework',
+    'research',    
     ]
 
 MIDDLEWARE = [
