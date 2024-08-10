55f8519f8eccfc7a1cbd272c27eceb028d5f3b83
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-08-10 06:29:18 +0200

    chore(fix): Reconfigured the settings_module using best practices

--- server/core/config/base.py
+++ server/core/config/base.py
@@ -18,8 +18,8 @@ load_dotenv()
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
