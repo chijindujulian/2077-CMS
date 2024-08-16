c307de1dcfb6daa65dc24780243a4fb20b824a1c
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-08-16 04:31:18 +0200

    chore(fix): Reconfigured redis-server to listen on port 6378

--- server/core/config/celery_config.py
+++ server/core/config/celery_config.py
@@ -1,6 +1,6 @@
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Redis as a broker
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6378/0'  # Redis as a broker
+CELERY_RESULT_BACKEND = 'redis://localhost:6378/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
