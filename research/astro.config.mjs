bb7094da80dc90377ca5df4eacb03da8bd713b19
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-08-20 10:22:03 -0300

    chore: setup svelte

--- research/astro.config.mjs
+++ research/astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   output: "server",
   darkMode: 'selector',
   adapter: node({
     mode: "standalone"
-  }),
+  })
 });
