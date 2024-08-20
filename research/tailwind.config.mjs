a5128a79baa48bbb0c8596a15e49b53b1ea667a5
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-08-20 10:37:53 -0300

    chore: config prettier

--- research/tailwind.config.mjs
+++ research/tailwind.config.mjs
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			black: '#020302',
-			white: '#efefef',
-			green: '#C6FF50',
-			greenLm: '#91b208'
-		},
-		fontFamily: {
-			sans: ['Graphik', 'sans-serif'],
-			serif: ['Merriweather', 'serif'],
-		},
-		extend: {
-			spacing: {
-				'8xl': '96rem',
-				'9xl': '128rem',
-			},
-			borderRadius: {
-				'4xl': '2rem',
-			},
-			fontFamily: {
-				avenir: ['Avenir', 'sans-serif'],
-			},
-		},
-	},
-};
+    darkMode: 'class',
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        colors: {
+            black: '#020302',
+            white: '#efefef',
+            green: '#C6FF50',
+            greenLm: '#91b208',
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '8xl': '96rem',
+                '9xl': '128rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            },
+            fontFamily: {
+                avenir: ['Avenir', 'sans-serif'],
+            },
+        },
+    },
+}
