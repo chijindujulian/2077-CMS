c889e25cb417aa676a1f5bdd274f082d5fc732a1
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-08-23 18:57:24 -0300

    chore: config vercel analytics

--- research/astro.config.mjs
+++ research/astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
+import svelte from '@astrojs/svelte'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  output: 'server',
-  darkMode: 'selector',
-  adapter: vercel(),
-  devToolbar: {
-    enabled: false
-  }
-});
+    integrations: [tailwind(), svelte()],
+    output: 'server',
+    darkMode: 'selector',
+    adapter: vercel(),
+    devToolbar: {
+        enabled: false,
+    },
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: { enabled: true },
+    }),
+})
